d5e19cabca022ded5b650f32dda4638d377cbc26
Abhi Yerra <abhi@opszero.com>
2024-08-28 13:49:51 -0700

    Fix

--- variables.tf
+++ variables.tf
@@ -3,12 +3,12 @@ variable "users" {
   type = map(object({
     first_name = string
     last_name  = string
-    groups     = list(string)
   }))
 }
 
 variable "groups" {
   type = map(object({
     description = string
+    users       = list(string)
   }))
 }
