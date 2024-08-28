8021a73bd7127e778323f8336d475736790aea0b
Abhi Yerra <abhi@opszero.com>
2024-08-28 13:51:48 -0700

    toset

--- group/main.tf
+++ group/main.tf
@@ -30,7 +30,7 @@ data "aws_identitystore_user" "this" {
 }
 
 resource "aws_identitystore_group_membership" "this" {
-  for_each = var.users
+  for_each = toset(var.users)
 
   identity_store_id = var.identity_store_id
   group_id          = aws_identitystore_group.this[each.key].group_id
