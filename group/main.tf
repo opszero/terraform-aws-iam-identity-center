b1459a496a7609965298d8ed3313b9cf319a9c97
Abhi Yerra <abhi@opszero.com>
2024-08-28 13:57:14 -0700

    Fix

--- group/main.tf
+++ group/main.tf
@@ -33,7 +33,7 @@ resource "aws_identitystore_group_membership" "this" {
   for_each = toset(var.users)
 
   identity_store_id = var.identity_store_id
-  group_id          = aws_identitystore_group.this[each.key].group_id
+  group_id          = aws_identitystore_group.this.group_id
 
   member_id = data.aws_identitystore_user.this[each.key].id
 }
